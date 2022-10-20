99836f53109d9ad97977ee7061b76a9fb664c308
elvinaliyev6 <elvinaliyevinfo@gmail.com>
2022-10-20 17:06:29 +0400

    first commit

---
+++ src/main/java/az/company/paydaytrade/PaydayTradeApplication.java
@@ -0,0 +1,13 @@
+package az.company.paydaytrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaydayTradeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaydayTradeApplication.class, args);
+    }
+
+}
